ecaea8ffeed1cdb2b92fbbbe190cc7759acd7a4d
EmreKolbakir <147906210+EmreKolbakir@users.noreply.github.com>
2023-11-07 22:52:22 +0300

    mouse_subscriber.cpp

--- src/mouse_node_EmreKolbakir/src/mouse_subscriber.cpp
+++ src/mouse_node_EmreKolbakir/src/mouse_subscriber.cpp
@@ -1,9 +1,12 @@
 #include "ros/ros.h"
 #include "std_msgs/UInt8MultiArray.h"
 
+// Callback function that will be called when a new message is received on the
+// "/mouse_node" topic
 void mouseCallback(const std_msgs::UInt8MultiArray::ConstPtr& msg)
 {
     ROS_INFO_STREAM("Mouse data received: ");
+    // Iterate through the received data and print each byte
     for (int i = 0; i < msg->data.size(); ++i) {
         std::cout << (int)msg->data[i] << " ";
     }
@@ -12,11 +15,15 @@ void mouseCallback(const std_msgs::UInt8MultiArray::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
+    // Initialize the ROS node with the name "mouse_subscriber"
     ros::init(argc, argv, "mouse_subscriber");
     ros::NodeHandle nh;
 
+    // Create a subscriber object that subscribes to the "/mouse_node" topic
+    // The mouseCallback function is called when new data is received
     ros::Subscriber sub = nh.subscribe("/mouse_node", 1000, mouseCallback);
 
+    // Enter a loop that processes callbacks and keeps the node running
     ros::spin();
 
     return 0;
